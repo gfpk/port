57572fe07e67a6c908f382d4d1ac9b7966440238
NCI <nci@nci-VirtualBox.(none)>
2014-10-27 19:17:03 +0000

    form page working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :works
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
